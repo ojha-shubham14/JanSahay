e6e23fca3b04578ceb3163d3eaff864d61ec8450
Shubham Ojha <shubham4clgg@gmail.com>
2026-09-14 15:05:03 +0530

    VITE: Modification

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/JanSahay/', // added for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
